5606c17ef7de43aac331ff38d24354ca1a0e48e6
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-07-26 19:52:24 +0700

    🐛 Fix google result
    - Fix wrong URL in google result
    - Block URL from banners

--- blockGoogleResult.js
+++ blockGoogleResult.js
@@ -1,38 +1,66 @@
-const result = document.getElementsByClassName('g');
+const results = document.getElementsByClassName("g");
+const latests = document.querySelectorAll(`[role=listitem]`);
 
 const blockResult = async (result, blockList) => {
-  if (result.classList.length > 1) {
-    return '';
-  }
+    if (result.classList.length > 1) {
+        return "";
+    }
 
-  let mainDivPosition = 0;
-  if (result.children[0].localName === 'h2') {
-    mainDivPosition = 1;
-  }
+    const topDiv = ["h2", "link"];
+    let mainDivPosition = 0;
+    if (topDiv.includes(result.children[0].localName)) {
+        mainDivPosition = 1;
+    }
 
-  let urlDom =
-    result.children[mainDivPosition].children[0].children[0].children[1]
-      .children[0].children[0];
+    let urlDom =
+        result.children[mainDivPosition].children[0].children[0].children[1]
+            .children[0].children[0];
 
-  if (urlDom.children.length === 2) {
-    urlDom = urlDom.children[0].children[2].children[0];
-  }
+    if (urlDom.children.length === 2) {
+        urlDom = urlDom.children[0].children[2].children[0];
+    }
 
-  const url = new URL('https://' + urlDom.childNodes[0].nodeValue);
+    const url = new URL(urlDom.childNodes[0].nodeValue);
 
-  if (blockList.includes(url.hostname)) {
-    result.style.display = 'none';
-  }
+    if (blockList.includes(url.hostname)) {
+        result.style.display = "none";
+    }
 };
 
-chrome.storage.sync.get('google', (googleBlocks) => {
-  const numberResult = result.length;
-  const blockResults = [];
-  const blockList = Object.values(googleBlocks['google']);
+const blockLatest = async (latest, blockList) => {
+    let firstChild = latest.children[0];
+    if (firstChild.localName !== "g-inner-card") {
+        firstChild = firstChild.children[0];
+    }
 
-  for (let i = 0; i < numberResult; i++) {
-    blockResults.push(blockResult(result[i], blockList));
-  }
+    const urlDom =
+        firstChild.children[0].children[0].children[0].children[1].children[0]
+            .children[1].childNodes[0].nodeValue;
 
-  Promise.all(blockResults).then(() => {});
+    const fullUrlDom =
+        "https://" + urlDom.charAt(0).toLowerCase() + urlDom.slice(1);
+    const url = new URL(fullUrlDom);
+
+    if (blockList.includes(url.hostname)) {
+        latest.style.display = "none";
+    }
+};
+
+chrome.storage.sync.get("google", (googleBlocks) => {
+    const numberResult = results.length;
+    const numberLatest = latests.length;
+    const blockResults = [];
+    const blockList = Object.values(googleBlocks["google"]);
+
+    for (let i = 0; i < numberResult; i++) {
+        blockResults.push(blockResult(results[i], blockList));
+    }
+
+    if (numberLatest > 0) {
+        for (let i = 0; i < numberLatest; i++) {
+            blockResults.push(blockLatest(latests[i], blockList));
+        }
+    }
+
+    Promise.all(blockResults).then(() => {});
 });
