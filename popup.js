9df7e2277bcbcda7a06877f905777d061290170b
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-01 12:01:12 +0700

    🎨 Format code

--- popup.js
+++ popup.js
@@ -1,80 +1,157 @@
-const listBox = document.getElementById('list');
+const listBox = document.getElementById("list");
 
 const checkEmptyObject = (obj) => {
-  for (let _key in obj) {
-    return false;
-  }
+    for (let _key in obj) {
+        return false;
+    }
 
-  return true;
+    return true;
 };
 
 let id = 0;
-chrome.storage.sync.get('google', (items) => {
-  console.log(items);
-  for (const key in items['google']) {
-    id = key;
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-    item.setAttribute('data-id', id);
+chrome.storage.sync.get("google", (items) => {
+    for (const key in items["google"]) {
+        id = key;
 
-    const input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    input.setAttribute('data-id', id);
-    input.setAttribute('class', 'inputBox');
-    input.value = items['google'][id];
-    input.style.display = 'none';
+        const item = document.createElement("div");
+        item.setAttribute("class", "item");
+        item.setAttribute("data-id", id);
 
-    input.addEventListener('blur', function () {
-      const itemId = this.getAttribute('data-id');
-      const displayBox = document.querySelector(
-        `.displayBox[data-id='${itemId}']`
-      );
+        const input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("data-id", id);
+        input.setAttribute("class", "inputBox");
+        input.value = items["google"][id];
+        input.style.display = "none";
 
-      displayBox.innerHTML = this.value;
-      if (displayBox.style.display == 'none') {
-        this.style.display = 'none';
-        displayBox.style.display = 'flex';
-      }
+        input.addEventListener("blur", function () {
+            const itemId = this.getAttribute("data-id");
+            const displayBox = document.querySelector(
+                `.displayBox[data-id='${itemId}']`
+            );
 
-      chrome.storage.sync.get('google', (google) => {
-        if (!checkEmptyObject(google)) {
-          google = google['google'];
+            displayBox.innerHTML = this.value;
+            if (displayBox.style.display == "none") {
+                this.style.display = "none";
+                displayBox.style.display = "flex";
+            }
+
+            chrome.storage.sync.get("google", (google) => {
+                if (!checkEmptyObject(google)) {
+                    google = google["google"];
+                }
+                google[itemId] = this.value;
+                chrome.storage.sync.set({ google });
+            });
+        });
+
+        const display = document.createElement("div");
+        display.setAttribute("data-id", id);
+        display.setAttribute("class", "displayBox");
+        display.style.display = "flex";
+        display.innerHTML = items["google"][key];
+
+        display.addEventListener("click", function () {
+            const itemId = this.getAttribute("data-id");
+            const inputBox = document.querySelector(
+                `input[data-id='${itemId}']`
+            );
+
+            this.style.display = "none";
+            inputBox.style.display = "block";
+            inputBox.focus();
+        });
+
+        const trash = document.createElement("img");
+        trash.setAttribute("data-id", id);
+        trash.setAttribute("src", "bin.svg");
+        trash.setAttribute("class", "trash");
+
+        trash.addEventListener("click", function () {
+            const itemId = this.getAttribute("data-id");
+
+            chrome.storage.sync.get("google", (google) => {
+                google = google["google"];
+                delete google[itemId];
+                chrome.storage.sync.set({ google });
+            });
+            const itemBox = document.querySelector(
+                `.item[data-id='${itemId}']`
+            );
+            itemBox.style.display = "none";
+        });
+
+        item.appendChild(input);
+        item.appendChild(display);
+        item.appendChild(trash);
+
+        list.appendChild(item);
+    }
+});
+
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", () => {
+    id++;
+    const item = document.createElement("div");
+    item.setAttribute("class", "item");
+    item.setAttribute("data-id", id);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("data-id", id);
+    input.setAttribute("class", "inputBox");
+
+    input.addEventListener("blur", function () {
+        const itemId = this.getAttribute("data-id");
+        const displayBox = document.querySelector(
+            `.displayBox[data-id='${itemId}']`
+        );
+
+        displayBox.innerHTML = this.value;
+        if (displayBox.style.display == "none") {
+            this.style.display = "none";
+            displayBox.style.display = "flex";
         }
-        google[itemId] = this.value;
-        chrome.storage.sync.set({ google });
-      });
+
+        chrome.storage.sync.get("google", (google) => {
+            if (!checkEmptyObject(google)) {
+                google = google["google"];
+            }
+
+            google[itemId] = this.value;
+            chrome.storage.sync.set({ google });
+        });
     });
 
-    const display = document.createElement('div');
-    display.setAttribute('data-id', id);
-    display.setAttribute('class', 'displayBox');
-    display.style.display = 'flex';
-    display.innerHTML = items['google'][key];
+    const display = document.createElement("div");
+    display.setAttribute("data-id", id);
+    display.setAttribute("class", "displayBox");
+    display.style.display = "none";
 
-    display.addEventListener('click', function () {
-      const itemId = this.getAttribute('data-id');
-      const inputBox = document.querySelector(`input[data-id='${itemId}']`);
+    display.addEventListener("click", function () {
+        const itemId = this.getAttribute("data-id");
+        const inputBox = document.querySelector(`input[data-id='${itemId}']`);
 
-      this.style.display = 'none';
-      inputBox.style.display = 'block';
-      inputBox.focus();
+        this.style.display = "none";
+        inputBox.style.display = "block";
+        inputBox.focus();
     });
 
-    const trash = document.createElement('img');
-    trash.setAttribute('data-id', id);
-    trash.setAttribute('src', 'bin.svg');
-    trash.setAttribute('class', 'trash');
+    const trash = document.createElement("img");
+    trash.setAttribute("data-id", id);
+    trash.setAttribute("src", "bin.svg");
+    trash.setAttribute("class", "trash");
 
-    trash.addEventListener('click', function () {
-      const itemId = this.getAttribute('data-id');
+    trash.addEventListener("click", function () {
+        const itemId = this.getAttribute("data-id");
 
-      chrome.storage.sync.get('google', (google) => {
-        google = google['google'];
-        delete google[itemId];
-        chrome.storage.sync.set({ google });
-      });
-      const itemBox = document.querySelector(`.item[data-id='${itemId}']`);
-      itemBox.style.display = 'none';
+        chrome.storage.sync.get("google", (google) => {
+            google = google["google"];
+            delete google[itemId];
+            chrome.storage.sync.set({ google });
+        });
+        const itemBox = document.querySelector(`.item[data-id='${itemId}']`);
+        itemBox.style.display = "none";
     });
 
     item.appendChild(input);
@@ -82,80 +159,7 @@ chrome.storage.sync.get('google', (items) => {
     item.appendChild(trash);
 
     list.appendChild(item);
-  }
-});
 
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', () => {
-  id++;
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-  item.setAttribute('data-id', id);
-
-  const input = document.createElement('input');
-  input.setAttribute('type', 'text');
-  input.setAttribute('data-id', id);
-  input.setAttribute('class', 'inputBox');
-
-  input.addEventListener('blur', function () {
-    const itemId = this.getAttribute('data-id');
-    const displayBox = document.querySelector(
-      `.displayBox[data-id='${itemId}']`
-    );
-
-    displayBox.innerHTML = this.value;
-    if (displayBox.style.display == 'none') {
-      this.style.display = 'none';
-      displayBox.style.display = 'flex';
-    }
-
-    chrome.storage.sync.get('google', (google) => {
-      if (!checkEmptyObject(google)) {
-        google = google['google'];
-      }
-
-      google[itemId] = this.value;
-      chrome.storage.sync.set({ google });
-    });
-  });
-
-  const display = document.createElement('div');
-  display.setAttribute('data-id', id);
-  display.setAttribute('class', 'displayBox');
-  display.style.display = 'none';
-
-  display.addEventListener('click', function () {
-    const itemId = this.getAttribute('data-id');
-    const inputBox = document.querySelector(`input[data-id='${itemId}']`);
-
-    this.style.display = 'none';
-    inputBox.style.display = 'block';
+    const inputBox = document.querySelector(`input[data-id='${id}']`);
     inputBox.focus();
-  });
-
-  const trash = document.createElement('img');
-  trash.setAttribute('data-id', id);
-  trash.setAttribute('src', 'bin.svg');
-  trash.setAttribute('class', 'trash');
-
-  trash.addEventListener('click', function () {
-    const itemId = this.getAttribute('data-id');
-
-    chrome.storage.sync.get('google', (google) => {
-      google = google['google'];
-      delete google[itemId];
-      chrome.storage.sync.set({ google });
-    });
-    const itemBox = document.querySelector(`.item[data-id='${itemId}']`);
-    itemBox.style.display = 'none';
-  });
-
-  item.appendChild(input);
-  item.appendChild(display);
-  item.appendChild(trash);
-
-  list.appendChild(item);
-
-  const inputBox = document.querySelector(`input[data-id='${id}']`);
-  inputBox.focus();
 });
